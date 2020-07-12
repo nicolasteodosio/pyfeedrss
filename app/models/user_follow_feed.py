8a6d0dde48540089bbfe6d024da6cabebfba52a4
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-12 18:25:30 -0300

    feature: creating user_follow_feed model

---
+++ app/models/user_follow_feed.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from app.models.base import BaseModel
+from app.models.feed import Feed
+
+
+class UserFollowFeed(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    feed = models.ForeignKey(Feed, on_delete=models.DO_NOTHING)
+    disabled_at = models.DateTimeField("Disabled at")
+
+    def __str__(self):
+        return f"{self.user}: {self.feed}"
