4130d2075def8a8ee03c64b093c23d3bfac2f86b
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-12 14:46:09 -0300

    fix: secret key exposed

--- pyfeedrss/settings.py
+++ pyfeedrss/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "gh_)fvjb#8cki_1r25-d=lu@5#l&&3$mn$w2ez2-o)=dubts$u"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
