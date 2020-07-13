b689109ac6550047ee8f33859901a826fcca2874
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-13 18:58:08 -0300

    refactor: adding docstring in base

--- app/models/base.py
+++ app/models/base.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    """BaseModel class created to implement fields that all models will use
+
+    """
+
     created_at = models.DateTimeField("Created at", auto_now_add=True)
     modified_at = models.DateTimeField("Modified at", auto_now_add=True)
 
