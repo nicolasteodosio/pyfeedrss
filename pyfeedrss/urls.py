2513fbde79c8f2d6e629117915d86c6b871d7171
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-13 21:37:39 -0300

    feature: adding urls

--- pyfeedrss/urls.py
+++ pyfeedrss/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    url(r"^home/$", views.home, name="home"),
+    url(r"^signup/$", views.signup, name="signup"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
