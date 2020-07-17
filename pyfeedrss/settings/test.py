52d27561ad48964a66f802c5e159fc6109805362
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-17 16:23:49 -0300

    feature: adding broker for tests

--- pyfeedrss/settings/test.py
+++ pyfeedrss/settings/test.py
@@ -4,3 +4,17 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="TEST",)
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["test"] = ({"NAME": "test_db"},)
+
+DRAMATIQ_BROKER = {
+    "BROKER": "dramatiq.brokers.stub.StubBroker",
+    "OPTIONS": {},
+    "MIDDLEWARE": [
+        "dramatiq.middleware.AgeLimit",
+        "dramatiq.middleware.TimeLimit",
+        "dramatiq.middleware.Callbacks",
+        "dramatiq.middleware.Pipelines",
+        "dramatiq.middleware.Retries",
+        "django_dramatiq.middleware.AdminMiddleware",
+        "django_dramatiq.middleware.DbConnectionsMiddleware",
+    ],
+}
