aa6382d8842e1e722a960845f0f26560454b04d0
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-19 11:40:45 -0300

    fix: typo

--- app/models/feed.py
+++ app/models/feed.py
@@ -6,7 +6,7 @@ from app.models.base import BaseModel
 class Feed(BaseModel):
     title = models.CharField("Title", max_length=100)
     link = models.URLField("Link")
-    descritpion = models.TextField("Description")
+    description = models.TextField("Description")
     ttl = models.IntegerField("TTL")
     last_build_date = models.DateTimeField("Last Build Date")
 
--- app/models/item.py
+++ app/models/item.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
     feed = models.ForeignKey(Feed, on_delete=models.DO_NOTHING)
     title = models.CharField("Title", max_length=100)
     link = models.URLField("Link")
-    descritpion = models.TextField("Description")
+    description = models.TextField("Description")
     published_at = models.DateTimeField("Published Date")
 
     def __str__(self) -> str:
