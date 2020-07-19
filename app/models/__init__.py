b703ab4d74eff8ef7925cc360c1f6b5e8e06da1d
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-19 13:18:59 -0300

    fix: adding models to init

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.feed import Feed  # noqa: F401
+from app.models.item import Item  # noqa: F401
+from app.models.user_follow_feed import UserFollowFeed  # noqa: F401
+from app.models.user_rel_item import UserRelItem  # noqa: F401
