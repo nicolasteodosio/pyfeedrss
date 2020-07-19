ef45e32ed10550ee98c9b889d7b034c179c33b2f
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-19 13:16:42 -0300

    fix: foreign key should be item and creating model manager

--- app/models/user_rel_item.py
+++ app/models/user_rel_item.py
@@ -1,18 +1,30 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import QuerySet
 from djchoices import ChoiceItem, DjangoChoices
 
 from app.models.base import BaseModel
-from app.models.feed import Feed
+from app.models.item import Item
+
+
+class UserRelItemKind(DjangoChoices):
+    comment = ChoiceItem("comment", "Comment")
+    read = ChoiceItem("read", "Read")
+
+
+class UserCommentItemManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(kind=UserRelItemKind.comment)
+
+
+class UserReadItemManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(kind=UserRelItemKind.read)
 
 
 class UserRelItem(BaseModel):
-    class UserRelItemKind(DjangoChoices):
-        comment = ChoiceItem("comment", "Comment")
-        read = ChoiceItem("read", "Read")
-
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    feed = models.ForeignKey(Feed, on_delete=models.DO_NOTHING)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
     kind = models.CharField("Kind", max_length=50, choices=UserRelItemKind.choices)
     content = models.TextField("Content", null=True, blank=True)
 
