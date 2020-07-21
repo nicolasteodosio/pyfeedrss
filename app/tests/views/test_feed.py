e3aeea1d951789fc2073d37b1e068fb9c48a77ff
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-21 20:17:22 -0300

    feature: creating tests for list feed view

---
+++ app/tests/views/test_feed.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+import pytest
+from django.shortcuts import resolve_url
+from model_mommy import mommy
+
+from app.models import Feed, UserFollowFeed
+
+
+def test_feed_list_view_user_not_logged(client):
+    response = client.get(resolve_url("list_feed"))
+    assert response.status_code == 302
+    assert response.url == "/accounts/login/?next=/feed/list/"
+
+
+def test_feed_list_view_empty(logged_client):
+    response = logged_client.get(resolve_url("list_feed"))
+    assert response.status_code == 200
+    assert response.resolver_match.url_name == "list_feed"
+    assert list(response.context["feeds_followed"]) == []
+    assert list(response.context["feeds_unfollowed"]) == []
+
+
+@pytest.mark.django_db
+def test_feed_list_view(logged_client):
+    feeds = mommy.make(Feed, _quantity=2)
+    user_id_ = int(logged_client.session._session["_auth_user_id"])
+    mommy.make(UserFollowFeed, feed_id=feeds[0].id, user_id=user_id_)
+    mommy.make(
+        UserFollowFeed,
+        feed_id=feeds[1].id,
+        user_id=user_id_,
+        disabled_at=datetime.now(),
+    )
+    response = logged_client.get(resolve_url("list_feed"))
+    assert response.status_code == 200
+    assert response.resolver_match.url_name == "list_feed"
+    assert response.context["feeds_followed"].count() == 1
+    assert response.context["feeds_unfollowed"].count() == 1
