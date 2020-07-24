fbe2ccb6677254800f119128b484046a1a4c128e
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-24 12:16:07 -0300

    feature: adding dramatiq decorator and max retries

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import dramatiq
 import feedparser
 
 from app.models.feed import Feed
@@ -8,7 +9,10 @@ from app.models.user_follow_feed import UserFollowFeed
 
 DATE_FORMAT = "%a, %d %b %Y %X %z"
 
+MAX_RETRIES = 3
 
+
+@dramatiq.actor(max_retries=MAX_RETRIES)
 def parse_feed(url: str, alias: str, user_id: int) -> None:
     """
 
@@ -17,23 +21,27 @@ def parse_feed(url: str, alias: str, user_id: int) -> None:
     :param user_id:
     :return:
     """
-    parsed_feed = feedparser.parse(url)
-    parsed_feed = parsed_feed["feed"]
-    feed_dict = {
-        "link": parsed_feed["links"][1]["href"],
-        "descritpion": parsed_feed["link"],
-        "ttl": parsed_feed["ttl"],
-        "last_build_date": datetime.strptime(parsed_feed["updated"], DATE_FORMAT,),
-    }
-    feed_object, created = Feed.objects.get_or_create(
-        title=parsed_feed["title"], defaults=feed_dict
-    )
+    try:
+        parsed_feed = feedparser.parse(url)
+        parsed_feed = parsed_feed["feed"]
+        feed_dict = {
+            "link": parsed_feed["links"][1]["href"],
+            "descritpion": parsed_feed["link"],
+            "ttl": parsed_feed["ttl"],
+            "last_build_date": datetime.strptime(parsed_feed["updated"], DATE_FORMAT,),
+        }
+        feed_object, created = Feed.objects.get_or_create(
+            title=parsed_feed["title"], defaults=feed_dict
+        )
 
-    follow_feed(feed_object.id, user_id)
-    if created:
-        parse_entries(url, feed_object.id)
+        follow_feed.send(feed_object.id, user_id)
+        if created:
+            parse_entries.send(url, feed_object.id)
+    except Exception as e:
+        raise e
 
 
+@dramatiq.actor(max_retries=MAX_RETRIES)
 def follow_feed(feed_id: int, user_id: int) -> None:
     """
 
@@ -41,9 +49,13 @@ def follow_feed(feed_id: int, user_id: int) -> None:
     :param user_id:
     :return:
     """
-    UserFollowFeed.objects.create(feed_id=feed_id, user_id=user_id)
+    try:
+        UserFollowFeed.objects.create(feed_id=feed_id, user_id=user_id)
+    except Exception as e:
+        raise e
 
 
+@dramatiq.actor(max_retries=MAX_RETRIES)
 def parse_entries(url: str, feed_id: int) -> None:
     """
 
@@ -51,54 +63,68 @@ def parse_entries(url: str, feed_id: int) -> None:
     :param feed_id:
     :return:
     """
-    parsed_entries = feedparser.parse(url)
-    parsed_entries = parsed_entries["entries"]
-    entries_to_create = []
-    for entry in parsed_entries:
-        entries_to_create.append(
-            Item(
-                feed_id=feed_id,
-                title=entry["title"],
-                link=entry["link"],
-                descritpion=entry["summary"],
-                published_at=datetime.strptime(entry["published"], DATE_FORMAT),
+    try:
+        parsed_entries = feedparser.parse(url)
+        parsed_entries = parsed_entries["entries"]
+        entries_to_create = []
+        for entry in parsed_entries:
+            entries_to_create.append(
+                Item(
+                    feed_id=feed_id,
+                    title=entry["title"],
+                    link=entry["link"],
+                    descritpion=entry["summary"],
+                    published_at=datetime.strptime(entry["published"], DATE_FORMAT),
+                )
             )
-        )
 
-    Item.objects.bulk_create(entries_to_create)
+        Item.objects.bulk_create(entries_to_create)
+    except Exception as e:
+        raise e
 
 
+@dramatiq.actor(max_retries=MAX_RETRIES)
 def update_feed(feed_id: int) -> None:
     """
 
     :param feed_id:
     :return:
     """
-    feed = Feed.objects.get(id=feed_id)
-    recent_item_date = (
-        Item.objects.filter(feed_id=feed.id).earliest("published_at").published_at
-    )
+    try:
+        feed = Feed.objects.get(id=feed_id)
 
-    parsed_feed = feedparser.parse(feed.link)
-    parsed_entries = parsed_feed["entries"]
-
-    filtered_entries = [
-        d
-        for d in parsed_entries
-        if datetime.strptime(d["published"], DATE_FORMAT) > recent_item_date
-    ]
-    entries_to_create = []
-
-    for entry in filtered_entries:
-        entry_published = datetime.strptime(entry["published"], DATE_FORMAT)
-        entries_to_create.append(
-            Item(
-                feed_id=feed_id,
-                title=entry["title"],
-                link=entry["link"],
-                descritpion=entry["summary"],
-                published_at=entry_published,
-            )
+        recent_item_date = (
+            Item.objects.filter(feed_id=feed.id).earliest("published_at").published_at
         )
 
-    Item.objects.bulk_create(entries_to_create)
+        parsed_feed = feedparser.parse(feed.link)
+
+        feed.last_build_date = (
+            datetime.strptime(parsed_feed["updated"], DATE_FORMAT,),
+        )
+        feed.save()
+
+        parsed_entries = parsed_feed["entries"]
+
+        filtered_entries = [
+            d
+            for d in parsed_entries
+            if datetime.strptime(d["published"], DATE_FORMAT) > recent_item_date
+        ]
+        entries_to_create = []
+
+        for entry in filtered_entries:
+            entry_published = datetime.strptime(entry["published"], DATE_FORMAT)
+            entries_to_create.append(
+                Item(
+                    feed_id=feed_id,
+                    title=entry["title"],
+                    link=entry["link"],
+                    descritpion=entry["summary"],
+                    published_at=entry_published,
+                )
+            )
+
+        Item.objects.bulk_create(entries_to_create)
+    except Exception as e:
+        raise e
