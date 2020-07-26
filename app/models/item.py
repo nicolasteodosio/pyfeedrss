20757eb9ef6f8c7d31d074316cf951b6dc57e28c
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-26 13:25:23 -0300

    refactor: removing unused property

--- app/models/item.py
+++ app/models/item.py
@@ -1,5 +1,3 @@
-from typing import Any, Optional
-
 from django.db import models
 
 from app.models.base import BaseModel
@@ -19,56 +17,3 @@ class Item(BaseModel):
 
     def __str__(self) -> str:
         return f"{self.title}"
-
-    @property
-    def read(self) -> bool:
-        """Property to check if the item was read
-
-        Returns
-        -------
-        bool:
-            A boolean if the item was read or not
-        """
-        from app.models.user_rel_item import UserRelItem
-
-        return UserRelItem.read.filter(item_id=self.id).exists()
-
-    @property
-    def commented(self) -> bool:
-        """Property to check if the item was commented
-
-        Returns
-        -------
-        bool:
-            A boolean if the item has a comment or not
-        """
-        from app.models.user_rel_item import UserRelItem
-
-        return UserRelItem.commented.filter(item_id=self.id).exists()
-
-    @property
-    def comment(self) -> Optional[Any]:
-        """Property to return the comment in the item
-
-        Returns
-        -------
-        The comment in the item or None
-        """
-        if self.commented:
-            from app.models.user_rel_item import UserRelItem
-
-            return UserRelItem.commented.get(item_id=self.id).content
-        return None
-
-    @property
-    def favorite(self) -> bool:
-        """Property to check if the item was favorited
-
-        Returns
-        -------
-        bool:
-            A boolean if the item was favorited or not
-        """
-        from app.models.user_rel_item import UserRelItem
-
-        return UserRelItem.favorite.filter(item_id=self.id).exists()
