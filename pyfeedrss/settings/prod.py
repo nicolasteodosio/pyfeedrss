bd83de5bbfd84d85867eca3dde6df67bd7f67351
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-07-27 23:40:21 -0300

    feature: preparing settings for prod

---
+++ pyfeedrss/settings/prod.py
@@ -0,0 +1,20 @@
+from .base import *  # noqa
+from .base import env
+
+DEBUG = False
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa F405
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
