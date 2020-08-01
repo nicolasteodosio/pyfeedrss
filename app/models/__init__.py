a8e3a39665272a09304e4e0952fd194d33fa71eb
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-08-01 17:18:43 -0300

    fix: model import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.feed import Feed  # noqa: F401
 from app.models.item import Item  # noqa: F401
+from app.models.notification import Notification  # noqa: F401
 from app.models.user_follow_feed import UserFollowFeed  # noqa: F401
 from app.models.user_rel_item import UserRelItem  # noqa: F401
