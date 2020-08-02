166be1eb11dcee5725223007a2d17b794d5c2ea5
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-08-02 17:08:03 -0300

    feature: adding notification in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Feed, Item, UserFollowFeed, UserRelItem
+from app.models import Feed, Item, Notification, UserFollowFeed, UserRelItem
 
 admin.site.register(Feed)
 admin.site.register(Item)
 admin.site.register(UserRelItem)
 admin.site.register(UserFollowFeed)
+admin.site.register(Notification)
