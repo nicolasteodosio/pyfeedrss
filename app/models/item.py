3ecb4a66a9a4c274682fba04e3a51991ab0e2a59
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2020-08-02 17:06:09 -0300

    refactor: migrating create_items to manager in item model

--- app/models/item.py
+++ app/models/item.py
@@ -1,11 +1,45 @@
+from datetime import datetime
+from time import mktime
+from typing import List
+
 from django.db import models
 
 from app.models.base import BaseModel
 from app.models.feed import Feed
 
 
+class ItemManager(models.Manager):
+    def bulk_entries_create(self, feed_id: int, parsed_entries: List) -> None:
+        """Function responsible for bulk_creating items parsed in tasks
+
+        Parameters
+        ----------
+        feed_id: int
+            Feed id
+        parsed_entries: List
+            List of feed entries to be created
+        Returns
+        -------
+
+        """
+        entries_to_create = []
+        for entry in parsed_entries:
+            entries_to_create.append(
+                Item(
+                    feed_id=feed_id,
+                    title=entry["title"],
+                    link=entry["link"],
+                    description=entry["summary"],
+                    published_at=datetime.fromtimestamp(
+                        mktime(entry["published_parsed"])
+                    ),
+                )
+            )
+        self.bulk_create(entries_to_create)
+
+
 class Item(BaseModel):
-    """Item model calss
+    """Item model class
 
     """
 
@@ -14,6 +48,7 @@ class Item(BaseModel):
     link = models.URLField("Link")
     description = models.TextField("Description")
     published_at = models.DateTimeField("Published Date")
+    objects = ItemManager()
 
     def __str__(self) -> str:
         return f"{self.title}"
